b3605201a05a84adcb8c2ee5dfda2587bc9e4ce2
Mecasu <melihcansunay@live.com>
2024-06-05 15:19:06 +0300

    wip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,39 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1,"Juliet", "Sellers" ));
+        employees.add(new Employee(2,"Raina", "Singleton" ));
+        employees.add(new Employee(3,"Anika", "Williamson" ));
+        employees.add(new Employee(4,"Angelina", "Fleming" ));
+        employees.add(new Employee(5,"Nylah", "Haynes" ));
+        employees.add(new Employee(6,"Gary", "Warner" ));
+        employees.add(new Employee(3,"Anika", "Williamson" ));
+        employees.add(new Employee(4,"Angelina", "Fleming" ));
+
+        public static List<Employee> findDuplicates(){
+
+
+        }
+        public static List<Employee> findUniques(){
+
+
+        }
+        public static List<Employee> removeDuplicates(){
+
+
+        }
+
+
     }
 }
---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,50 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return getId() == employee.getId() && Objects.equals(getFirstName(), employee.getFirstName()) && Objects.equals(getLastName(), employee.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
